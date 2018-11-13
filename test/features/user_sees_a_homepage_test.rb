75842329bbb3cb1a6c90822b7043e9da6f6b6068
Leah Miller <lkmiller.87@gmail.com>
2018-11-12 17:16:26 -0700

    Adds about.html and refactor for displaying about page

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -7,4 +7,11 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+
+  def test_it_creates_404_message_if_page_does_not_exist
+    visit '/irish'
+
+    assert page.has_content?('Page not found.')
+    assert_equal 404, page.status_code
+  end
 end
